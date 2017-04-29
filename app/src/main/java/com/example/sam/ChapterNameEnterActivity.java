1ebf2baffa6e93a18bf546c5e07b068ea2d2ddad
Mexator <brisilant@yandex.ru>
2017-04-29 20:06:56 +0300

    Replaced Layout in MainActivity. Add new CreateNewChapterActivity

---
+++ app/src/main/java/com/example/sam/ChapterNameEnterActivity.java
@@ -0,0 +1,15 @@
+package com.example.sam;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ChapterNameEnterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapter_name_enter);
+
+    }
+
+}
