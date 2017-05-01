9f2e8a4ccde53335c61415dd3d6a013ed6895d46
Mexator <brisilant@yandex.ru>
2017-05-01 14:45:02 +0300

    Realized Creating of new Chapters. Detected bug: new tabs disappearing with screen rotation.

--- app/src/main/java/com/example/sam/ChapterNameEnterActivity.java
+++ app/src/main/java/com/example/sam/ChapterNameEnterActivity.java
@@ -1,15 +1,50 @@
 package com.example.sam;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class ChapterNameEnterActivity extends AppCompatActivity {
+public class ChapterNameEnterActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button CancelButton;
+    Button OKButton;
+    EditText EnterChapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chapter_name_enter);
 
+        CancelButton = (Button)findViewById(R.id.chapter_create_cancel_button);
+        OKButton = (Button)findViewById(R.id.chapter_create_ok_button);
+        EnterChapter = (EditText)findViewById(R.id.edit_chapter_name);
+
+        CancelButton.setOnClickListener(this);
+        OKButton.setOnClickListener(this);
     }
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.chapter_create_cancel_button:
+            {
+                Intent answerIntent = new Intent();
+                setResult(RESULT_CANCELED,answerIntent);
+                finish();
+            }
+            case R.id.chapter_create_ok_button:
+            {
+                Intent answerIntent = new Intent();
+                String ChapterName = EnterChapter.getText().toString();
+                answerIntent.putExtra("ChapterName",ChapterName);
+                setResult(RESULT_OK,answerIntent);
+                finish();
+            }
+        }
+    }
+
 
 }
--- app/src/main/java/com/example/sam/MainActivity.java
+++ app/src/main/java/com/example/sam/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.sam;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TabHost;
-import android.widget.TabHost.OnTabChangeListener;
 import com.googlecode.tesseract.android.TessBaseAPI;
 
-public class MainActivity extends Activity implements View.OnClickListener{
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
     TabHost Tabs;
     Button button;
     //Tabs.setOnTabChangedListener();
@@ -22,10 +22,11 @@ public class MainActivity extends Activity implements View.OnClickListener{
         Tabs.setup();//Включаем TabHost
 
         button = (Button)findViewById(R.id.button);
-        button.setOnClickListener((View.OnClickListener)this);
+        button.setOnClickListener(this);
 
         TabHost.TabSpec tabSpec = Tabs.newTabSpec("tag1");//Создаем первую вкладку
         tabSpec.setContent(R.id.tab1);
+
         tabSpec.setIndicator("Заметки");
         Tabs.addTab(tabSpec);
 
@@ -37,15 +38,26 @@ public class MainActivity extends Activity implements View.OnClickListener{
         switch (view.getId())
         {
             case R.id.button:
-                AddNewPage("fdgdfg");
+            {
+                Intent intent = new Intent(MainActivity.this,ChapterNameEnterActivity.class);
+                startActivityForResult(intent,2);
+
+            }
         }
     }
-    private void AddNewPage(String name)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(data==null) return;
+        String Name = data.getStringExtra("ChapterName");
+        AddNewChapter(Name);
+    }
+
+    private void AddNewChapter(String name)
     {
         TabHost.TabSpec tabSpec = Tabs.newTabSpec("new_tab");
         tabSpec.setContent(R.id.empty_tab);
         tabSpec.setIndicator(name);
         Tabs.addTab(tabSpec);
-
     }
 }
